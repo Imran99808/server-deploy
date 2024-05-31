56a8f3a223668241d8a7bbd0588f97ad2a24e560
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-05-31 03:34:56 -0700

    Update dog.js

--- src/api/dog/controllers/dog.js
+++ src/api/dog/controllers/dog.js
@@ -137,8 +137,8 @@ ctx.body={
          httpOnly: true,
          secure: true,
          maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          domain:"client-deploy-rho.vercel.app",
-        //  sameSite: "none",
+         domain:"client-deploy-rho.vercel.app",
+         sameSite: "none",
          // overwrite: true,
       });
   
