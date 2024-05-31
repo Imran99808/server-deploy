1b96278346960e3c2d2d666c1e077f4aa3ba292f
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-05-31 03:54:43 -0700

    Update middlewares.js
    cors update

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,14 +15,24 @@ module.exports = [
       },
     },
   },
-   'strapi::cors',
+   // 'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-
+ // ...
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [https://client-deploy-rho.vercel.app'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  // ...
 
   
 ];
