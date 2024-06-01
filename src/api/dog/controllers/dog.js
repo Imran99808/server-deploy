3fb4dd00f951070d9219b9f84382b547ab2e16ed
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-06-01 08:36:35 -0700

    Update dog.js
    okk

--- src/api/dog/controllers/dog.js
+++ src/api/dog/controllers/dog.js
@@ -134,12 +134,12 @@ ctx.body={
       }
       const token=jwt.sign(tokenInfo,process.env.CLIENT_SECRET_JWT);
      
-      ctx.cookies.set('jwt','emon', {
+      ctx.cookies.set('jwt',token, {
          httpOnly: true,
          secure: true,
          maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
          domain:'.client-deploy-rho.vercel.app',
-         
+         sameSite:'none',
       });
   
 
