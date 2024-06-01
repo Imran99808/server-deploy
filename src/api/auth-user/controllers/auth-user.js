4eb9275e9d5124d1cb58f2f065d081b981bab8ff
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-06-01 07:30:03 -0700

    Update auth-user.js
    testing

--- src/api/auth-user/controllers/auth-user.js
+++ src/api/auth-user/controllers/auth-user.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const jwt=require('jsonwebtoken');
 /**
  * auth-user controller
  */
@@ -21,6 +21,27 @@ module.exports = createCoreController('api::auth-user.auth-user',({strapi})=>({
          
           
         }catch(error){
+
+const tokenInfo={
+            userId:'s',
+            email:'r',
+            image:'s'
+          }
+          const token=jwt.sign(tokenInfo,'ondex');
+         
+          ctx.cookies.set('jwt',token, {
+            httpOnly: true,
+            // secure: true,
+             maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
+             domain:'.client-deploy-rho.vercel.app',
+            // sameSite: "none",
+            overwrite: true,
+          });
+      
+
+
+
+          
           ctx.status=401;
           ctx.body={
             massage:'unauthorized'
