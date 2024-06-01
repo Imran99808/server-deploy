ce46d48084e1b3352c8d1ba72845c552da55678b
Èmràñ Rèzà <103001711+Imran99808@users.noreply.github.com>
2024-06-01 09:04:56 -0700

    Update middlewares.js
    s

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,24 +15,24 @@ module.exports = [
       },
     },
   },
-  // 'strapi::cors',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
- // ...
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['https://client-deploy-20q1.onrender.com','https://server-deploy-6984.onrender.com'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+ //  ...
+ //  {
+ //    name: 'strapi::cors',
+ //    config: {
+ //      origin: ['https://client-deploy-20q1.onrender.com','https://server-deploy-6984.onrender.com'],
+ //      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+ //      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       
-      keepHeaderOnError: true,
-    },
-  },
+ //      keepHeaderOnError: true,
+ //    },
+ //  },
   // ...
 
   
